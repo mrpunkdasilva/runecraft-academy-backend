5d23eaf8f0ca40e6ecc300b1995b70bf83a42930
Mr Punk da Silva <gustavojs417@gmail.com>
2024-11-14 10:44:57 -0300

    feat: iniciando projeto

---
+++ src/main/java/org/equipe4/flashpomobackend/FlashpomoBackendApplication.java
@@ -0,0 +1,13 @@
+package org.equipe4.flashpomobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashpomoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashpomoBackendApplication.class, args);
+    }
+
+}
