40cc946a26b77b41e8d7497caaeebcf44a82b80b
rhuan_me <rhuan.esteves0724@jala.university>
2024-11-14 17:28:36 -0300

    created Ensemble Class

--- src/main/java/org/equipe4/flashpomobackend/models/Ensemble.java
+++ src/main/java/org/equipe4/flashpomobackend/models/Ensemble.java
@@ -1,7 +1,25 @@
 package org.equipe4.flashpomobackend.models;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Ensemble {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
 
-
+    @Lob
+    @Column(name = "image", columnDefinition = "LONGBLOB")
+    private byte[] cover;
 }
