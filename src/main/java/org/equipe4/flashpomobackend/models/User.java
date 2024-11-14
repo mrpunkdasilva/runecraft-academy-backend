5775850abf95ed46b298eea127f61ea092f5ccb6
Mr Punk da Silva <gustavojs417@gmail.com>
2024-11-14 15:57:26 -0300

    feat: started creation of models

---
+++ src/main/java/org/equipe4/flashpomobackend/models/User.java
@@ -0,0 +1,37 @@
+package org.equipe4.flashpomobackend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String password;
+    private String email;
+    private String rule;
+    
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
