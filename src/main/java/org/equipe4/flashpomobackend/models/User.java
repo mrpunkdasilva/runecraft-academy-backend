e163988c78e5c433d21a08e6b079eb16f2095cf1
Mr Punk da Silva <gustavojs417@gmail.com>
2024-11-14 22:15:13 -0300

    feat: added model User

--- src/main/java/org/equipe4/flashpomobackend/models/User.java
+++ src/main/java/org/equipe4/flashpomobackend/models/User.java
@@ -1,24 +1,45 @@
 package org.equipe4.flashpomobackend.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
+import java.security.Timestamp;
+
 @ToString
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
+@Entity
 public class User {
-    @Id
+   @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @Column(name = "USER_ID")
+    private Integer userId;
+
+    @Column(name = "NAME", length = 200)
     private String name;
+
+    @Column(name = "PASSWORD", length = 90)
     private String password;
+
+    @Column(name = "EMAIL", length = 150, unique = true)
     private String email;
-    private String rule;
+
+    @Column(name = "ROLE", length = 10)
+    private String role;
+
+    @Lob
+    @Column(name = "AVATAR")
+    private byte[] avatar;
+
+    @Column(name = "CREATED_AT")
+    private Timestamp createdAt;
+
+    @Column(name = "UPDATED_AT")
+    private Timestamp updatedAt;
+
+    @Column(name = "STATUS")
+    private boolean status;
 }
