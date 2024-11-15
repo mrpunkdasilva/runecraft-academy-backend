58111d6d6a18aecbebc8d1c82f8b3a77d1fc6e1c
Mr Punk da Silva <gustavojs417@gmail.com>
2024-11-15 00:17:15 -0300

    docs(models): added Ensemble

--- src/main/java/org/equipe4/flashpomobackend/models/Ensemble.java
+++ src/main/java/org/equipe4/flashpomobackend/models/Ensemble.java
@@ -1,24 +1,40 @@
 package org.equipe4.flashpomobackend.models;
 
-
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@ToString
-@Getter
-@Setter
+/**
+ * The Ensemble class represents an ensemble entity in the application.
+ * It is annotated with Lombok annotations for generating getters, setters, constructors, and other boilerplate code.
+ * The class is also annotated as an entity, indicating that it maps to a database table.
+ */
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
+@Builder
+@Entity
+@Table(name = "ENSEMBLE")
 public class Ensemble {
-
+    /**
+     * The unique identifier of the ensemble, which is the primary key.
+     * It is generated automatically using the IDENTITY strategy.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    /**
+     * The name of the ensemble.
+     */
     private String name;
+
+    /**
+     * The description of the ensemble.
+     */
     private String description;
 
+    /**
+     * The cover image of the ensemble, stored as a byte array.
+     */
     @Lob
     @Column(name = "image", columnDefinition = "LONGBLOB")
     private byte[] cover;
