b525680bfe692a4804c7002577f9a9a245b4be17
Gustavo Henrique de Jesus da Silva <gustavo.jesus0724@jala.university>
2024-11-27 11:30:26 -0300

    docs: Update controller documentation

--- src/main/java/org/equipe4/flashpomobackend/repository/EnsembleRepository.java
+++ src/main/java/org/equipe4/flashpomobackend/repository/EnsembleRepository.java
@@ -3,6 +3,13 @@ package org.equipe4.flashpomobackend.repository;
 import org.equipe4.flashpomobackend.models.Ensemble;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for managing Ensemble entities.
+ * This interface extends JpaRepository to provide CRUD operations and other database functionalities for Ensemble objects.
+ *
+ * @param <Ensemble> the type of entity to be managed, in this case, Ensemble
+ * @param <Integer> the type of the entity's primary key, in this case, Integer
+ */
 public interface EnsembleRepository extends JpaRepository<Ensemble, Integer> {
 
 }
--- src/main/java/org/equipe4/flashpomobackend/repository/FlashcardRepository.java
+++ src/main/java/org/equipe4/flashpomobackend/repository/FlashcardRepository.java
@@ -3,5 +3,12 @@ package org.equipe4.flashpomobackend.repository;
 import org.equipe4.flashpomobackend.models.Flashcard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * This interface represents a repository for managing Flashcard entities in the database.
+ * It extends the JpaRepository interface provided by Spring Data JPA, which provides
+ * basic CRUD (Create, Read, Update, Delete) operations and additional query methods.
+ *
+ * @since 1.0
+ */
 public interface FlashcardRepository extends JpaRepository<Flashcard, Integer> {
 }
